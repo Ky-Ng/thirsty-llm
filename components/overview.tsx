987c020a231f63d6cf52e651b17cab5414140b1b
Kyle Ng <kgng@usc.edu>
2024-11-26 11:47:27 -0800

    add changes for sustainability project

--- components/overview.tsx
+++ components/overview.tsx
@@ -19,32 +19,23 @@ export const Overview = () => {
           <span>+</span>
           <MessageIcon size={32} />
         </p>
-        <p>
-          This is an{' '}
-          <Link
-            className="font-medium underline underline-offset-4"
-            href="https://github.com/vercel/ai-chatbot"
-            target="_blank"
-          >
-            open source
-          </Link>{' '}
-          chatbot template built with Next.js and the AI SDK by Vercel. It uses
-          the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">streamText</code>{' '}
-          function in the server and the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">useChat</code> hook
-          on the client to create a seamless chat experience.
+        <p className="flex flex-row justify-center gap-4 items-center">
+          <strong>How Thirsty is Your LLM?</strong>
         </p>
-        <p>
-          You can learn more about the AI SDK by visiting the{' '}
-          <Link
+        <p className="flex flex-row justify-center gap-4 items-center">
+          Visualize the estimated environmental impact of your AI + LLM usage in real-time
+        </p>
+        <p className="flex flex-row justify-center gap-4 items-center">
+          Ask "What's my environmental impact"
+          to see the natural resource consumption of your current conversation
+        </p>
+        <p className="flex flex-row justify-center gap-4 items-center">
+          Checkout the <Link
             className="font-medium underline underline-offset-4"
             href="https://sdk.vercel.ai/docs"
             target="_blank"
-          >
-            docs
-          </Link>
-          .
+          >writeup</Link>
+          for more background and info.
         </p>
       </div>
     </motion.div>
